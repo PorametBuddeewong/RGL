fbc2f3758799723cf360f28f8b827b56a6b2d2f4
PorametBuddeewong <129479901+PorametBuddeewong@users.noreply.github.com>
2026-09-11 11:44:17 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -238,16 +238,16 @@ def calculate_rgl_rayong(suppliers, rgl_p_usd):
         
         logistics_cost_usd = feed * logistics_cost_usd_mt_cgl
         
-        n2_cost_usd = feed * 5.37803 * (n2_p / fx)
-        cfw_cost_usd = feed * 0.84234 * (water_p / fx)
-        dmw_cost_usd = feed * 0.09955 * dmw_p_usd
-        bfw_cost_usd = feed * 0.32987 * bfw_p_usd
+        n2_cost_usd = feed *  5.51772 * (n2_p / fx)
+        cfw_cost_usd = feed *  1.34300 * (water_p / fx)
+        dmw_cost_usd = feed *  0.13351  * dmw_p_usd
+        bfw_cost_usd = feed *  0.33844 * bfw_p_usd
 
         util1_cost = n2_cost_usd + cfw_cost_usd + dmw_cost_usd + bfw_cost_usd
         
-        naoh_cost_usd = feed * 0.02356 * (naoh_p * 1000 / fx) 
-        fac_cost_usd = feed * 0.00088 * fac_p_usd
-        rac_cost_usd = feed * 0.00377 * rac_p_usd
+        naoh_cost_usd = feed * 0.02456 * (naoh_p * 1000 / fx) 
+        fac_cost_usd = feed * 0.00091 * fac_p_usd
+        rac_cost_usd = feed * 0.00387 * rac_p_usd
         chem1_cost = naoh_cost_usd + fac_cost_usd + rac_cost_usd
         
         variable_costs_usd = util1_cost + chem1_cost
@@ -260,8 +260,8 @@ def calculate_rgl_rayong(suppliers, rgl_p_usd):
         
         total_vc_usd = variable_costs_usd + ovc_total_usd + dis1_cost
         
-        ygl_credit_usd = feed * 0.1450 * ygl_p_usd
-        gl_res_credit_usd = feed * 0.0360 * gl_res_p_usd
+        ygl_credit_usd = feed * 0.1250 * ygl_p_usd
+        gl_res_credit_usd = feed * 0.0060 * gl_res_p_usd
         cgl_80_credit_usd = feed * 0.0358 * cgl_80_p_usd
         
         total_bp_credit_usd = ygl_credit_usd + gl_res_credit_usd + cgl_80_credit_usd
